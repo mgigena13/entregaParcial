763fb0975da32aeaa6c6da7ea09b17047c30a803
Martin Gigena Curchod <magc13@hotmail.com>
2021-11-17 22:56:44 -0300

    actualizacion 20211117b

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ const carrito = [];
     arrProductos.push (new Producto ('HP ProBook 455R G6 9VX50ES R3-3200U 8GB - 256GB SSD 15', 'Informatica', '39.6 cm (15.6") Notebook, Windows 10 Pro. Display: 1920 x 1080, IPS. AMD Ryzen 3 3200U 2x 2.60 GHz. AMD Radeon Vega 3 Mobile Shared Memory. 32 GB RAM, 256 GB SSD. USB 3.1 Gen1 Typ C, 2x USB 3.1 Gen1, Wi-Fi 5 (802.11ac)', 284050, true, 4, 'INF1890N', 2));
     arrProductos.push (new Producto ('Microsoft Office 2019 Home & Business', 'Informatica', 'Windows, Mac OS. Standard -Full Version. Serial number without disc', 18900, false, 80, 'INF9800S', 3));
 
-const verProductos = () => {
+const verProductos = (arrProductos) => {
     let visual = '';
     for (const itProducto of arrProductos) {
         visual += `Producto: ${itProducto.nombre}\nPrecio: $${itProducto.precio}\nID: ${itProducto.id}\n\n\n`
@@ -26,14 +26,23 @@ const verProductos = () => {
 }
 
 const comprar = () => {
-    const compra = Number(prompt(`Ingresa ID del producto a comprar\n\n ${verProductos()}`));
+    const compra = Number(prompt(`Ingresa ID del producto a comprar\n\n ${verProductos(arrProductos)}`));
     const productoEncontrado = arrProductos.find(producto => producto.id === compra);
     if (productoEncontrado){
-        carrito.push(productoEncontrado)
+        carrito.push(productoEncontrado);
         alert(`Agregaste ${productoEncontrado.nombre} a tu carrito`);
         console.log(carrito)        
     } else {
         alert('Producto inexistente')
+    }
+}
+
+const mostrarCarrito = () => {
+    const productosCarrito = verProductos(carrito);
+    if (productosCarrito){
+        alert (productosCarrito);
+    }else{
+        alert('El carrito esta vacio')
     }
 }
 
@@ -48,7 +57,7 @@ do{
 
     switch (opciones) {
         case 1:
-            const valorRec = verProductos();
+            const valorRec = verProductos(arrProductos);
             alert(valorRec);
             break;
         case 2:
